974557d5663b1a2fdf4d174052d39dc9f64f040a
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-12 19:54:29 +0000

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,31 @@
-import { open } from 'lmdb';
-import { DatabaseAdapter } from './classes/DatabaseAdapter.js';
 import express from 'express';
-
-// const db = open<number, string>('db', { useVersions: true });
-const db = open<number, string>('db', {});
+import { open } from 'lmdb';
 
 const app = express();
-console.log('running!');
+const db = open<number, string>('db', { useVersions: true });
+const key = 'counter';
+
 app.get('/', async (req, res) => {
-    let int: number | undefined;
-    // Between 3x - 4x as slow!
-    // let success;
-    // do {
-    //     let results = db.getEntry('count');
-    //     int = (results?.value || 0) + 1;
-    //     let version = results?.version;
-    //     let nextVersion = (version || 0) + 1;
-    //     console.log({ results, int, version, nextVersion });
-    //     success = await db.put('count', int, nextVersion, version);
-    //     console.log({ success });
-    // } while (!success);
-    await new Promise((resolve) => setTimeout(resolve, Math.random() * 100));
-    await db.transaction(() => {
-        int = (db.get('count') || 0) + 1;
-        db.put('count', int);
-    });
-    await new Promise((resolve) => setTimeout(resolve, Math.random() * 100));
-    if (undefined === int) {
+    let value: number | undefined;
+    let success;
+    do {
+        let results = db.getEntry(key);
+        value = (results?.value || 0) + 1;
+        let version = results?.version;
+        let nextVersion = (version || 0) + 1;
+        success = await db.put(key, value, nextVersion, version);
+    } while (!success);
+    if (undefined === value) {
         res.status(500).send(
             JSON.stringify({
-                error: 'Undefined token',
+                error: 'Undefined value',
             })
         );
         return;
     }
     res.send(
         JSON.stringify({
-            token: int,
+            value,
         })
     );
 });
