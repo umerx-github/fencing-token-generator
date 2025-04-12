88502fcb1c6682e6e20688d759e4ecf8540150c7
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-12 20:20:19 +0000

    Make safe

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,21 @@ const app = express();
 const db = open<number, string>('db', { useVersions: true });
 const key = 'counter';
 
+// Required - ifVersion is still undefined when there is possibility for concurrent readers/writers, can cause conflicts.
+let results = db.getEntry(key);
+if (undefined === results) {
+    await db.put(key, 0, 0);
+}
+
 app.get('/', async (req, res) => {
     let value: number | undefined;
     let success;
     do {
         let results = db.getEntry(key);
         value = (results?.value || 0) + 1;
-        let version = results?.version;
-        let nextVersion = (version || 0) + 1;
-        success = await db.put(key, value, nextVersion, version);
+        let ifVersion = results?.version;
+        let nextVersion = (ifVersion || 0) + 1;
+        success = await db.put(key, value, nextVersion, ifVersion);
     } while (!success);
     if (undefined === value) {
         res.status(500).send(
