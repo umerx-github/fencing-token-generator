e6aa1f5b61b6f920c6184ec206ea917db319092e
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-12 19:43:38 +0000

    Do it without transactions

--- src/index.ts
+++ src/index.ts
@@ -7,36 +7,16 @@ const db = open<number, string>('db', {});
 
 const app = express();
 console.log('running!');
+const state = {
+    int: (await db.get('counter')) || 0,
+};
 app.get('/', async (req, res) => {
-    let int: number | undefined;
-    // Between 3x - 4x as slow!
-    // let success;
-    // do {
-    //     let results = db.getEntry('count');
-    //     int = (results?.value || 0) + 1;
-    //     let version = results?.version;
-    //     let nextVersion = (version || 0) + 1;
-    //     console.log({ results, int, version, nextVersion });
-    //     success = await db.put('count', int, nextVersion, version);
-    //     console.log({ success });
-    // } while (!success);
-    await new Promise((resolve) => setTimeout(resolve, Math.random() * 100));
-    await db.transaction(() => {
-        int = (db.get('count') || 0) + 1;
-        db.put('count', int);
-    });
-    await new Promise((resolve) => setTimeout(resolve, Math.random() * 100));
-    if (undefined === int) {
-        res.status(500).send(
-            JSON.stringify({
-                error: 'Undefined token',
-            })
-        );
-        return;
-    }
+    state.int++;
+    const val = state.int;
+    await db.put('counter', val);
     res.send(
         JSON.stringify({
-            token: int,
+            token: val,
         })
     );
 });
